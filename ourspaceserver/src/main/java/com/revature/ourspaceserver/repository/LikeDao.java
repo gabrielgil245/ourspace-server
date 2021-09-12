ef7e3568f0e097631802bc95250fcc2a40fae497
gabrielgil245 <gabrielgil245@gmail.com>
2021-09-11 22:48:34 -0500

    toggle like functionality created; gitignore h2testing folder implemented

--- ourspaceserver/src/main/java/com/revature/ourspaceserver/repository/LikeDao.java
+++ ourspaceserver/src/main/java/com/revature/ourspaceserver/repository/LikeDao.java
@@ -1,12 +1,22 @@
 package com.revature.ourspaceserver.repository;
 
 import com.revature.ourspaceserver.model.Like;
+import com.revature.ourspaceserver.model.Post;
+import com.revature.ourspaceserver.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository("likeDao")
 @Transactional
 public interface LikeDao extends JpaRepository<Like, Integer> {
-
+    Like findLikeByUserAndPost(User user, Post post);
+    @Modifying
+    @Query("Delete from Like where likeId = :likeId")
+    void deleteLike(@Param("likeId") Integer likeId);
 }
